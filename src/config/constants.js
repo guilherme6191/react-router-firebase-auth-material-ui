6ce64715d8943e123fd133df898b581ff80027c4
Guilherme6191 <gui.vasconcelos.21@gmail.com>
2017-11-20 18:40:09 -0200

    feat: add and show .env for firebase info.

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 require('firebase/firestore');
 
 const config = {
-  apiKey: '##',
-  authDomain: '##',
-  databaseURL: '##',
-  projectId: '##',
-  storageBucket: '##',
-  messagingSenderId: '##'
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
